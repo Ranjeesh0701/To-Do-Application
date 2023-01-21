7fe56c3961f5efc2d6e45b22bf21c4cc06dfee4c
Ranjeesh <ranjeesh@hippovideo.io>
2023-01-21 17:38:13 +0530

    added home, index files and set up navigation

---
+++ app/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './pages/Home';
+import Login from './pages/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+        {/* <Stack.Screen name="Home" component={Home} options={{title: 'Welcome'}} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
